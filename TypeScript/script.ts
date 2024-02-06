abb053b05b0da1557df9d8c957cfc00d4641ba4b
George <appeahg@outlook.com>
2024-02-06 12:20:08 +0000

    Corrected all of the errors

--- TypeScript/script.ts
+++ TypeScript/script.ts
@@ -4,12 +4,12 @@ function validateName1() {
     var nameInput = document.getElementById('name');
 
     // Trim the input value to remove leading and trailing spaces
-    var name = nameInput.value.trim();
+    var name = (nameInput as HTMLInputElement).value.trim();
 
     if (name === 'John Doe') {
-        nameInput.classList.add('valid');
+        nameInput!.classList.add('valid');
     } else {
-        nameInput.classList.remove('valid');
+        nameInput!.classList.remove('valid');
     }
 }
 
@@ -18,35 +18,38 @@ function validateCredit() {
     var CreditInput = document.getElementById('Credit');
 
     // Trim the input value to remove leading and trailing spaces
-    var Credit = CreditInput.value.replace(/\s/g, ''); // Remove spaces from the input
+    var Credit = (CreditInput as HTMLInputElement).value.replace(/\s/g, ''); // Remove spaces from the input
+
+
+    
 
     if (Credit.trim() === '') {
-        CreditInput.classList.remove('valid');
-        CreditInput.classList.remove('invalid');
+        CreditInput!.classList.remove('valid');
+        CreditInput!.classList.remove('invalid');
     } else if (/[^0-9]/.test(Credit) || Credit.length !== 16) {
-        CreditInput.classList.remove('valid');
-        CreditInput.classList.add('invalid');
+        CreditInput!.classList.remove('valid');
+        CreditInput!.classList.add('invalid');
     } else {
-        CreditInput.classList.remove('invalid');
-        CreditInput.classList.add('valid');
+        CreditInput!.classList.remove('invalid');
+        CreditInput!.classList.add('valid');
     }
 }
 
 function validateEmail() {
     var emailInput = document.getElementById('Email');
-    var email = emailInput.value.trim();
+    var email = (emailInput as HTMLInputElement).value.trim();
 
     if (email.trim() === '') {
-        emailInput.classList.remove('valid');
-        emailInput.classList.remove('invalid');
+        emailInput!.classList.remove('valid');
+        emailInput!.classList.remove('invalid');
     } else {
         var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (emailRegex.test(email)) {
-            emailInput.classList.remove('invalid');
-            emailInput.classList.add('valid');
+            emailInput!.classList.remove('invalid');
+            emailInput!.classList.add('valid');
         } else {
-            emailInput.classList.remove('valid');
-            emailInput.classList.add('invalid');
+            emailInput!.classList.remove('valid');
+            emailInput!.classList.add('invalid');
         }
     }
 }
@@ -54,7 +57,7 @@ function validateEmail() {
     // Trim the input value to remove leading and trailing spaces
     function validateForename() {
         var forenameInput = document.getElementById('forename');
-        var forename = forenameInput.value.trim();
+        var forename = (forenameInput as HTMLInputElement).value.trim();
     
         // Check for potential SQL injection commands
         var potentialSQLCommands = ["'", "IF", "OR", "AND", "IN"];
@@ -66,23 +69,23 @@ function validateEmail() {
         }
 
     if (forename === '') {
-        forenameInput.classList.remove('valid');
-        forenameInput.classList.remove('invalid');
+        forenameInput!.classList.remove('valid');
+        forenameInput!.classList.remove('invalid');
     } else {
         var nameRegex = /^[a-zA-Z]+$/;
         if (nameRegex.test(forename)) {
-            forenameInput.classList.remove('invalid');
-            forenameInput.classList.add('valid');
+            forenameInput!.classList.remove('invalid');
+            forenameInput!.classList.add('valid');
         } else {
-            forenameInput.classList.remove('valid');
-            forenameInput.classList.add('invalid');
+            forenameInput!.classList.remove('valid');
+            forenameInput!.classList.add('invalid');
         }
     }
 }
 
 function validateSurname() {
     var surnameInput = document.getElementById('surname');
-    var surname = surnameInput.value.trim();
+    var surname = (surnameInput as HTMLInputElement).value.trim();
 
     // Check for potential SQL injection commands
     var potentialSQLCommands = ["'", "IF", "OR", "AND", "IN"];
@@ -95,16 +98,16 @@ function validateSurname() {
 
 
     if (surname === '') {
-        surnameInput.classList.remove('valid');
-        surnameInput.classList.remove('invalid');
+        surnameInput!.classList.remove('valid');
+        surnameInput!.classList.remove('invalid');
     } else {
         var nameRegex = /^[a-zA-Z]+$/;
         if (nameRegex.test(surname)) {
-            surnameInput.classList.remove('invalid');
-            surnameInput.classList.add('valid');
+            surnameInput!.classList.remove('invalid');
+            surnameInput!.classList.add('valid');
         } else {
-            surnameInput.classList.remove('valid');
-            surnameInput.classList.add('invalid');
+            surnameInput!.classList.remove('valid');
+            surnameInput!.classList.add('invalid');
         }
     }
 }
